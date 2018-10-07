b6e9e7eb9681c5ec6fce31ac6351928aaa341611
Shane Caler <scaler0222@gmail.com>
2018-10-07 04:17:36 -0700

    Added basic movement and sounds

---
+++ Assets/Rocket.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ProcessInput();
+
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating, which is why this if statement is separated
+        {
+            if(!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward); // vector3.forward = z axis
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward); // clockwise
+        }
+    }
+}
