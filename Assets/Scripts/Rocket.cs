c3e3ef5a87e82b17cbc7a741dd41804d8abd5097
Shane Caler <scaler0222@gmail.com>
2018-10-08 01:19:09 -0700

    Finished core gameplay, built version 1 as WebGL

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 10f;
+    [SerializeField] float levelLoadDelay = 2f;
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem deathParticles;
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    bool collisionsDisabled = true;
+
+    bool isTransitioning = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isTransitioning)
+        {
+            ResondToThrustInput();
+            RespondToRotateInput();
+        }
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (isTransitioning) { return; } // if we are dead, ignore collisions
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                // do nothing
+                break;
+            case "Finish":
+                successParticles.Play();
+                StartSuccessSequence();
+                break;
+            default:
+                mainEngineParticles.Stop();
+                deathParticles.Play();
+                StartDeathSequence();
+                break;
+        }
+    }
+
+    private void StartSuccessSequence()
+    {
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        Invoke("LoadNextLevel", levelLoadDelay); // Invoke takes the function as a string and a time (1 sec)
+
+    }
+
+    private void StartDeathSequence()
+    {
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        Invoke("LoadFirstLevel", levelLoadDelay); // parameterise time
+    }
+
+    private void LoadNextLevel()
+    {
+        int currScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = (currScene + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextScene);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void ResondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating, which is why this if statement is separated
+        {
+            ApplyThrust();
+        }
+        else
+        {
+            StopApplyingThrust();
+        }
+    }
+
+    private void StopApplyingThrust()
+    {
+        audioSource.Stop();
+        mainEngineParticles.Stop();
+    }
+
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        mainEngineParticles.Play();
+    }
+
+    private void RespondToRotateInput()
+    {
+        rigidBody.angularVelocity = Vector3.zero; // remove rotation due to phsyics
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame); // vector3.forward = z axis
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame); // clockwise
+        }
+    }
+}
+    
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+    [SerializeField] float speed = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.left, speed * Time.deltaTime);
+    }
+}
